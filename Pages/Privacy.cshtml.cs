a1c99300f4e8ec39fda6ab2c15c55f2e87f78d77
LitenPanJun <xiaopanjun233@163.com>
2025-11-22 03:10:41 +0800

    chore: init dotnet razor pages template

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fantasy.MinecraftServerConsole.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
