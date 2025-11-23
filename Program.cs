4192f848ce4a0ba42a947e241dbcc079d5393402
LitenPanJun <xiaopanjun233@163.com>
2025-11-23 12:42:46 +0800

    feat(services): encapsulate Minecraft Server Services and register in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Fantasy.MinecraftServerConsole.Hubs;
+using Fantasy.MinecraftServerConsole.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
+builder.Services.AddHostedService<ServerService>();
 
 var app = builder.Build();
 
---
+++ Services/ServerService.cs
@@ -0,0 +1,43 @@
+﻿using Fantasy.MinecraftServerConsole.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace Fantasy.MinecraftServerConsole.Services
+{
+    public class ServerService(IHubContext<LogHub> hub) : BackgroundService
+    {
+        private Process? _mc;
+
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            await Task.Yield();
+            StartServer();   // java -jar server.jar
+            while (!ct.IsCancellationRequested)
+                await Task.Delay(1000, ct);
+        }
+
+        private void StartServer()
+        {
+            _mc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "java",
+                    Arguments = "-jar server.jar nogui",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            _mc.OutputDataReceived += (_, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(e.Data)) return;
+                // 实时推给 SignalR
+                hub.Clients.All.SendAsync("ReceiveLog", e.Data);
+            };
+            _mc.Start();
+            _mc.BeginOutputReadLine();
+        }
+    }
+}
